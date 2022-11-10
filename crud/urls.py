c91033b052a7cf5336b7698d2598f0ffc4c1c41d
Tuhin Hosean <43568783+Fnnytechnology@users.noreply.github.com>
2022-11-10 19:51:30 +0600

    Update urls.py

--- crud/urls.py
+++ crud/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path,include
-
+from django.cof import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('' ,include('fontend.urls'),name='fontend'),
     path("api/", include("api.urls") , name="api")
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
